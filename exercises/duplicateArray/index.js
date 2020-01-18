e98fd36e76108ccd445ec4eee97939f59deb05da
mlutfiibra <mlutfiibra@outlook.com>
2020-01-18 18:02:09 +0700

    Implementing duplicate array

--- exercises/duplicateArray/index.js
+++ exercises/duplicateArray/index.js
@@ -1,6 +1,7 @@
 function duplicatedArray(arr) {
   const hash = {};
   const uniqueArray = [];
+  // const duplicateCollection = [];
 
   for (let val of arr) {
     hash[val] = hash[val] + 1 || 1;
@@ -9,6 +10,11 @@ function duplicatedArray(arr) {
   for (let val in hash) {
     uniqueArray.push(val);
   }
+
+  //   collect duplicate value in hash
+  //   for (let val in hash) {
+  //     if (hash[val] > 1) duplicateCollection.push(val);
+  //   }
 
   return uniqueArray;
 }
