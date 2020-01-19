69a09930cadc387d9d78a9eeabb73c7199e9d711
mlutfiibra <mlutfiibra@outlook.com>
2020-01-19 19:13:28 +0700

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -5,15 +5,25 @@
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
 //   pyramid(1)
-//       '#'
+//       '#' // i=j
 //   pyramid(2)
-//       ' # '
-//       '###'
+//       ' # ' //i 2
+//       '###' //j === n-1 2 -> 3
 //   pyramid(3)
 //       '  #  '
-//       ' ### '
-//       '#####'
+//       ' ### ' j === 1 || j === 3 i === 1
+//       '#####' 3 -> 5
 
-function pyramid(n) {}
+function pyramid(n) {
+  let star = "";
+
+  for (let i = 1; i <= n; i++) {
+    star = "";
+    for (let j = 1; j <= n * 2 - 1; j++) {
+      j >= n + 1 - i && j <= n - 1 + i ? (star += "*") : (star += " ");
+    }
+    console.log(star);
+  }
+}
 
 module.exports = pyramid;
