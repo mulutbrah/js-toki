97859ffd2130dc20103b485e1674c92fe66032ce
mlutfiibra <mlutfiibra@outlook.com>
2020-01-27 07:53:32 +0700

    isSubstring builtinfunction and for loop

---
+++ exercises/isSubstring/index.js
@@ -0,0 +1,29 @@
+/*
+    Assume you have a method isSubstring which checks ifone word is asubstring of another. 
+    Given two strings, 51 and 52, write code to check if 52 is a rotation 
+    of 51 using only one call to isSubstring (e.g.,"waterbottle"is a rotation of"erbottlewat")
+*/
+
+function isSubstring(string1, string2) {
+  /*
+  1. Built in Function
+    return (
+      string1 ===
+      string2
+        .split("")
+        .reverse()
+        .join("")
+    );
+
+    */
+  /*
+  2. For Loop
+    let reverseStr = ""
+  
+    for(let i = string2.length - 1; i >= 0; i--) {
+        reverseStr += string2[i]
+    }
+  
+    return reverseStr === string1
+  */
+}
