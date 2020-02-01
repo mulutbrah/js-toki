3b5bbc7989c3e43ca0d8bb0373895cf9ab9847e8
Mulutbrah <mlutfiibra@outlook.com>
2020-02-01 13:29:25 +0700

    Finishing numberSwapper

---
+++ exercises/crackingCodingInterview/numberSwapper.js
@@ -0,0 +1,10 @@
+// Number Swapper: Write a function to swap a number in place (that is, without temporary variables)
+
+function numberSwapper() {
+  let a = 5;
+  let b = 3;
+
+  a = a + b; // 8
+  b = a - b; // 5
+  a = a - b; // 3
+}
