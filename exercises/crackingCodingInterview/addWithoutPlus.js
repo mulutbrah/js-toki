92a85460d6d12ec281a0adc4b0a9f40c81cb5dd3
Mulutbrah <mlutfiibra@outlook.com>
2020-02-02 00:18:00 +0700

    Tictactoe

---
+++ exercises/crackingCodingInterview/addWithoutPlus.js
@@ -0,0 +1,6 @@
+/*
+    Write a function that adds two numbers. 
+    You should not use + or any arithmetic operators.
+*/
+
+function addWithoutPlus(number1, number2) {}
--- exercises/crackingCodingInterview/ticTacWin.js
+++ exercises/crackingCodingInterview/ticTacWin.js
@@ -2,36 +2,155 @@
     Design an algorithm to figure out if someone has won a game of tic-tac-toe.
 */
 
-function titTacWin(table) {
-  const winnerMap = {};
+function ticTacWin(board) {
+  const winner = [];
+  let i = 0;
+  let j = 0;
 
-  for (let i = 0; i < table[i].length; i++) {
-    for (let j = 0; j < table[i].length; j++) {
-      if (table[i][j] === "O" || table[i][j] === "x") {
-        if (
-          (table[i][j - 1] && table[i][j - 1] === table[i][j]) ||
-          (table[i][j + 1] && table[i][j + 1] === table[i][j]) ||
-          (table[i][j + 2] && table[i][j + 2] === table[i][j]) ||
-          (table[i - 1][j - 1] && table[i - 1][j - 1] === table[i][j]) ||
-          (table[i][j - 1] && table[i][j - 1] === table[i][j]) ||
-          (table[i - 1][j - 2] && table[i - 1][j - 2] === table[i][j]) ||
-          (table[i - 1][j - 1] && table[i - 1][j - 1] === table[i][j]) ||
-          (table[i - 1][j] && table[i - 1][j] === table[i][j])
-        ) {
-          winnerMap.count = winnerMap.count + 1 || 1;
-          if (winnerMap.count === 3) {
-            return table[i][j];
-          }
-        }
-      }
+  while (i < 3 && j < 3) {
+    if (
+      j === 0 &&
+      table[i][j] &&
+      table[i][j + 1] &&
+      table[i][j + 1] === table[i][j] &&
+      table[i][j + 2] &&
+      table[i][j + 2] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
     }
+
+    if (
+      j === 1 &&
+      table[i][j] &&
+      table[i][j - 1] &&
+      table[i][j - 1] === table[i][j] &&
+      table[i][j + 1] &&
+      table[i][j + 1] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
+    }
+
+    if (
+      j === 2 &&
+      table[i][j] &&
+      table[i][j - 1] &&
+      table[i][j - 1] === table[i][j] &&
+      table[i][j - 2] &&
+      table[i][j - 2] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
+    }
+
+    if (
+      i === 0 &&
+      table[i][j] &&
+      table[i + 1][j] &&
+      table[i + 1][j] === table[i][j] &&
+      table[i + 2][j] &&
+      table[i + 2][j] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
+    }
+
+    if (
+      i === 1 &&
+      table[i][j] &&
+      table[i - 1][j] &&
+      table[i - 1][j] === table[i][j] &&
+      table[i + 1][j] &&
+      table[i + 1][j] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
+    }
+
+    if (
+      i === 2 &&
+      table[i][j] &&
+      table[i - 1][j] &&
+      table[i - 1][j] === table[i][j] &&
+      table[i - 2][j] &&
+      table[i - 2][j] === table[i][j]
+    ) {
+      !winner.includes(table[i][j]) && winner.push(table[i][j]);
+    }
+
+    if (j < 2) {
+      j += 1;
+    } else {
+      j = 0;
+      i += 1;
+    }
+  }
+
+  if (winner.length === 1) {
+    return winner[0];
+  } else if (winner.includes(0) || winner[0] === 0) {
+    return -1;
+  } else {
+    return 0;
   }
 }
 
+// console.log(
+//   ticTacWin([
+//     ["x", " ", "O"],
+//     [" ", " ", "O"],
+//     ["x", "x", "O"]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [1, 2, 1],
+//     [1, 1, 2],
+//     [2, 2, 0]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [2, 2, 2],
+//     [0, 1, 1],
+//     [1, 0, 0]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [2, 1, 2],
+//     [2, 1, 1],
+//     [1, 2, 1]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [0, 2, 2],
+//     [2, 1, 1],
+//     [0, 0, 1]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [1, 2, 0],
+//     [0, 1, 2],
+//     [0, 0, 1]
+//   ])
+// );
+
+// console.log(
+//   ticTacWin([
+//     [1, 2, 1],
+//     [1, 1, 2],
+//     [2, 1, 2]
+//   ])
+// );
+
 console.log(
-  titTacWin([
-    ["x", " ", "O"],
-    [" ", " ", "O"],
-    ["x", "x", "O"]
+  ticTacWin([
+    [0, 0, 2],
+    [0, 0, 0],
+    [1, 0, 1]
   ])
 );
