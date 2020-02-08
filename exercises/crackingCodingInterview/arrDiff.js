10079d28770e73dc98cebeeba442d7ff955de0ac
Mulutbrah <mlutfiibra@outlook.com>
2020-02-08 11:47:19 +0700

    Fix: Arr dif

--- exercises/crackingCodingInterview/arrDiff.js
+++ exercises/crackingCodingInterview/arrDiff.js
@@ -14,3 +14,21 @@
 function arrayDiff(a, b) {
   return a.filter(x => !b.includes(x));
 }
+
+/*
+    Test.describe("Sample tests", function() {
+    Test.it("Should pass Sample tests", function() {
+        Test.assertDeepEquals(arrayDiff([], [4, 5]), [], "a was [], b was [4,5]");
+        Test.assertDeepEquals(
+        arrayDiff([3, 4], [3]),
+        [4],
+        "a was [3,4], b was [3]"
+        );
+        Test.assertDeepEquals(
+        arrayDiff([1, 8, 2], []),
+        [1, 8, 2],
+        "a was [1,8,2], b was []"
+        );
+    });
+    });
+*/
