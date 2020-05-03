bc000de7dc779abea05fff79e61fbc03704947d2
mulutbrah <mlutfiibra@outlook.com>
2020-05-03 21:48:39 +0700

    Add mock_test

--- exercises/async_test/test.js
+++ exercises/async_test/test.js
@@ -9,3 +9,23 @@ it("should calls swapi to get people", () => {
     expect(data.count).toBeGreaterThan(5);
   });
 });
+
+it("should returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+
+  expect.assertions(4);
+  return getPeople(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.py4e.com/api/people");
+    expect(data.count).toEqual(87);
+    expect(data.count).toBeGreaterThan(5);
+  });
+});
