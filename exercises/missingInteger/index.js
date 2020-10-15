18c973de23fc92eff5ddd3587153828c1329e53d
mulutbrah <mlutfiibra@outlook.com>
2020-10-16 04:11:01 +0700

    add missinginteger ex

---
+++ exercises/missingInteger/index.js
@@ -0,0 +1,22 @@
+const missingInteger = (A) => {
+  // 1
+  //   const sorted = A.sort();
+  //   return sorted.reduce((acc, val) => (acc === val ? acc + 1 : acc), 1);
+
+  // 2
+  const map = {};
+
+  A.map((x) => {
+    map[x] = x;
+    return x;
+  });
+
+  let result = 1;
+
+  while (true) {
+    if (!map[result]) return result;
+    result++;
+  }
+};
+
+console.log(missingInteger([1, 3, 6, 4, 1, 2]));
