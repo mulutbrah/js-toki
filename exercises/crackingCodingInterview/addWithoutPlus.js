1d11484c73c711d6021fc5135ec23a131eaf9c28
mulutbrah <mlutfiibra@outlook.com>
2020-10-17 08:39:42 +0700

    Fix solution

--- exercises/crackingCodingInterview/addWithoutPlus.js
+++ exercises/crackingCodingInterview/addWithoutPlus.js
@@ -3,4 +3,16 @@
     You should not use + or any arithmetic operators.
 */
 
-function addWithoutPlus(number1, number2) {}
+function addWithoutPlus(number1, number2) {
+  while (number2 != 0) {
+    let carry = number1 & number2;
+
+    number1 = number1 ^ number2;
+
+    number2 = carry << 1;
+  }
+
+  return number1;
+}
+
+console.log(addWithoutPlus(4, 5));
